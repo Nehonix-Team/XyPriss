645af25dc978c9822f082f3c1a8ddc2dba9d7ef8
CodeLover <ndriassoumou74@gmail.com>
2026-01-11 22:30:02 +0000

    refactor(xypriss-sys): format file modification dates in listing output Convert file modification timestamps to human-readable format using chrono, displaying as YYYY-MM-DD HH:MM:SS, and adjust column width for better alignment.

---
+++ tools/xypriss-sys/target/debug/deps/autocfg-3f438ae49723d386.d
@@ -0,0 +1,10 @@
+/home/idevo/Documents/projects/XyPriss/tools/xypriss-sys/target/debug/deps/autocfg-3f438ae49723d386.d: /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/lib.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/error.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/rustc.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/version.rs
+
+/home/idevo/Documents/projects/XyPriss/tools/xypriss-sys/target/debug/deps/libautocfg-3f438ae49723d386.rlib: /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/lib.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/error.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/rustc.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/version.rs
+
+/home/idevo/Documents/projects/XyPriss/tools/xypriss-sys/target/debug/deps/libautocfg-3f438ae49723d386.rmeta: /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/lib.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/error.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/rustc.rs /home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/version.rs
+
+/home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/lib.rs:
+/home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/error.rs:
+/home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/rustc.rs:
+/home/idevo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/autocfg-1.5.0/src/version.rs:
