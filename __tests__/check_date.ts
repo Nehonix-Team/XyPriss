9a1248165e83ada9f3995c53bdd056d990a710a5
loan <loankl1728@gmail.com>
2026-01-12 13:47:48 +0000

    feat(sys): add file verification to server  File verification now requires specific file presence for server startup

--- __tests__/check_date.ts
+++ __tests__/check_date.ts
@@ -1,9 +1,9 @@
-import { XyPrissRunner } from "./src/sys/XyPrissRunner";
-import { SysApi } from "./src/sys/SysApi";
+import { XyPrissRunner } from "../src/sys/XyPrissRunner";
+import { SysApi } from "../src/sys/SysApi";
 
 const runner = new XyPrissRunner(process.cwd());
 const sys = new SysApi(runner);
- 
+
 const stats = sys.$stats("scripts/test-fs-full.ts");
 console.log("Raw Created:", stats.created);
 console.log("Date Object:", new Date(stats.created * 1000));
