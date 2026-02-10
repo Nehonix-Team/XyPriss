cf575742e3b5e7c7d62f86a24fc797da93d5f53d
loan <loankl1728@gmail.com>
2026-02-10 08:55:18 +0000

    docs: organized into logical subdirectories based on their content

--- .private/configs.ts
+++ .private/configs.ts
@@ -14,8 +14,8 @@ export const testSConfigs: ServerOptions = __const__.$make({
 export const testSConfigs2: ServerOptions = {
     security: {
         rateLimit: __const__.$make({
-            max: 2,
-            message: "this is a test rtlm msg for server 2",
+            max: 10,
+            message: "This is a test rtlm msg for server 2. You can update configs by reading this '.private/configs.ts' file",
         }),
     },
 };
