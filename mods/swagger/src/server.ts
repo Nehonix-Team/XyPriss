f12bd0baba70a7ef88286bada2445b11217a0200
CodeLover <ndriassoumou74@gmail.com>
2026-04-07 15:32:18 +0000

    fix(core): harden project root discovery stack matching and update swagger integration

--- mods/swagger/src/server.ts
+++ mods/swagger/src/server.ts
@@ -4,7 +4,7 @@ import { generateOpenAPI } from "./openapi";
 import { SwaggerConfig } from "./types";
 import { getSwaggerUIStream } from "./ui";
 import { Plugin } from "xypriss";
- 
+
 type auxis = NonNullable<
     ReturnType<typeof Plugin.create>["onAuxiliaryServerDeploy"]
 >;
@@ -21,7 +21,25 @@ export function SwaggerServer(
 ) {
     const docPath = config.path || "/docs";
     const specPath = `${docPath}/swagger.json`;
+    console.log("plugin root path: ", __sys__.__root__);
+
+    console.log(
+        "😇 env de HELLO depuis le plugin: ",
+        __sys__.__env__.get("HELLO"),
+    );
+    console.log(
+        "🤧 env de COMMON_VAR du root du project (devrait être undefined): ",
+        __sys__.__env__.get("COMMON_VAR"),
+    );
+
+    console.log(
+        "🤠 env from root using __sys__ (devrait être strictement undefined vue que ici, sys.__root__ devrait conduire vers '/home/idevo/Documents/projects/XyPriss/mods/swagger') pourtant le .env de cette root n'a pas de name: ",
+        __sys__.__env__.get("NAME"),
+    );
+
     const workspaceSYS = __sys__.plugins.get(meta.name);
+
+    // console.log("workspaceFS: ", workspaceFS);
 
     if (!workspaceSYS) {
         throw new Error(
@@ -29,7 +47,13 @@ export function SwaggerServer(
                 " is not authorized in your xypriss.config.jsonc or xypriss.config.json. Please add ",
         );
     }
-    
+    console.log(
+        "🤠 env from root using workspaceSYS (devrait être undefined selon la config de l'utilisateur: en mode ROOT:// ça devrait conduire vers _sys__.root ('/home/idevo/Documents/projects/XyPriss/mods/swagger') et CWD:// devrait envoyer process.cwd() ('/home/idevo/Documents/projects/XyPriss' (vue qu'on excute la cmd ici))): ",
+        workspaceSYS.__env__.get("NAME"),
+    );
+    console.log("😝 root path: ", workspaceSYS.__root__);
+    console.log("😅 listing directory path: ", workspaceSYS.fs.ls("."));
+
     const port = config.port || 7070;
     const server = ops.createAuxiliaryServer({
         server: { port },
@@ -78,5 +102,4 @@ export function SwaggerServer(
         );
         logger.http(`GET ${url}`);
     });
-
 }
